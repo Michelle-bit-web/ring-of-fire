fda5c477afd232023c396730d5873b3cc6a50b4e
Michelle <mpuschkarow@web.de>
2025-05-24 23:30:34 +0200

    game component build; own route for game set and connect to click function of start button

--- src/app/start-screen/start-screen.component.ts
+++ src/app/start-screen/start-screen.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-start-screen',
-  imports: [],
+  imports: [
+    RouterModule,
+  ],
   templateUrl: './start-screen.component.html',
   styleUrl: './start-screen.component.scss'
 })
 export class StartScreenComponent {
 
+    constructor(private router: Router){}
+
+  newGame(){
+    //Start game
+    this.router.navigate(['/game']);
+    console.log('newGame() started')
+  }
 }
