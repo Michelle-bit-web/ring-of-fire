5d5a9e8d46f2409f354da571e09127b80148795a
Michelle <mpuschkarow@web.de>
2025-05-25 09:37:56 +0200

    Game class build that is used by component to create new Object; for-loop fills the stack and randomly sort them

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Game } from '../../models/game';
+
 
 @Component({
   selector: 'app-game',
@@ -9,8 +11,19 @@ import { Component } from '@angular/core';
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
+
 export class GameComponent {
   pickCardAnimation = false;
+  game?: Game;
+
+  ngOnInit(): void{
+    this.newGame();
+  }
+
+  newGame(){
+    this.game = new Game();
+    console.log(this.game)
+  }
 
   takeCard(){
     this.pickCardAnimation = true;
