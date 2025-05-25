6de42d902bf8fdaf162c2cd877a0ed2a8b1290b1
Michelle <mpuschkarow@web.de>
2025-05-25 14:24:29 +0200

    players array from model used to display multiple players

--- src/models/game.ts
+++ src/models/game.ts
@@ -1,5 +1,5 @@
 export class Game {
-    public players: string[] = [];
+    public players: string[] = ["Gary", "Simon", "Jonas"];
     public stack: string[] = [];
     public playedCards: string[] = [];
     public currentPlayer: number = 0;
